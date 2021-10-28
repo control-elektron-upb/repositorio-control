b64a02794f1821113c80d29fe58adbe9efbaaf34
Andresfe55 <luqueandres55@gmail.com>
2021-10-28 16:38:14 -0500

    creación de función para el monitoreo de las varables

--- source/variable_monitoring/Variable_monitoring.h
+++ source/variable_monitoring/Variable_monitoring.h
@@ -16,26 +16,49 @@
 
 
 //valores minimos y maximos de cada parte
-int V_MAX_BMS = 0;
-int V_MIN_BMS = 0;
-int I_MAX_BMS = 0;
-int I_MIN_BMS = 0;
-int P_MAX_BMS = 0;
-int P_MIN_BMS = 0;
-int T_MAX_BMS = 0;
-int NIV_BAT_MIN_BMS = 0;
-int V_BAT_MAX_DCDC = 0;
-int V_BAT_MIN_DCDC = 0;
-int T_MAX_DCDC = 0 ;
-int VEL_MAX_MOTOR = 0;
-int T_MAX_MOTOR = 0;
-int V_MAX_INV = 0;
-int V_MIN_INV = 0;
-int I_MAX_INV = 0;
-int I_MIN_INV = 0;
-int T_MAX_INV = 0;
-int P_MAX_INV = 0;
-int P_MIN_INV = 0;
+int V_MAX_BMS           = 0;
+int V_MIN_BMS           = 0;
+
+int I_MAX_BMS           = 0;
+int I_MIN_BMS           = 0;
+
+int P_MAX_BMS           = 0;
+int P_MIN_BMS           = 0;
+
+int T_MAX_BMS           = 0;
+int T_MIN_BMS           = 0;
+
+int V_CELDA_MAX_BMS     = 0;
+int V_CELDA_MIN_BMS     = 0;    
+
+int NIV_BAT_MAX_BMS     = 0;
+int NIV_BAT_MIN_BMS     = 0;
+
+
+int V_BAT_MAX_DCDC      = 0;
+int V_BAT_MIN_DCDC      = 0;
+
+int T_MAX_DCDC          = 0;
+int T_MIN_DCDC          = 0;
+
+
+int VEL_MAX_INV       = 0;
+int VEL_MIN_INV       = 0;
+
+int T_MAX_MOTOR         = 0;
+int T_MIN_MOTOR         = 0;
+
+int V_MAX_INV           = 0;
+int V_MIN_INV           = 0;
+
+int I_MAX_INV           = 0;
+int I_MIN_INV           = 0;
+
+int T_MAX_INV           = 0;
+int T_MIN_INV           = 0;
+
+int P_MAX_INV           = 0;
+int P_MIN_INV           = 0;
 
 
 //estados posibles de cada parte
