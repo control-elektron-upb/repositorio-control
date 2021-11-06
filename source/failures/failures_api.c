ef38fb48859a23e0074ec7ecf0d0633fe8083d53
Juanes-22 <juanesgarciamar@outlook.com>
2021-11-06 02:28:37 -0500

    se agrego funcion de llenado de estructuras a failures_api

--- source/failures/failures_api.c
+++ source/failures/failures_api.c
@@ -5,6 +5,11 @@
 
 #include "failures_api.h"
 #include "failures_st.h"
+#include "def_structs.h"
+
+void Llenar_Structs(void) {
+    fill_structs();
+}
 
 uint8_t Leer_estado_failures(void) {
     return failures_st_leer();
--- source/failures/failures_api.h
+++ source/failures/failures_api.h
@@ -8,5 +8,6 @@
 
 void FAILURES_OPS(void);
 uint8_t Leer_estado_failures(void);
+void Llenar_Structs(void);
 
 #endif  /* _FAILURES_API_H_ */
