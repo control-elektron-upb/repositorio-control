4631829f4ed28f1f9b5d1ace2dab0fd3dfdd7e2a
Andresfe55 <luqueandres55@gmail.com>
2021-11-06 16:22:40 -0500

    cambio de variables a float en los maximos y minimos

--- source/variable_monitoring/Variable_monitoring.h
+++ source/variable_monitoring/Variable_monitoring.h
@@ -16,49 +16,49 @@
 
 
 //valores minimos y maximos de cada parte
-uint8_t V_MAX_BMS           = 0;
-uint8_t V_MIN_BMS           = 0;
+float V_MAX_BMS           = 0;
+float V_MIN_BMS           = 0;
 
-uint8_t I_MAX_BMS           = 0;
-uint8_t I_MIN_BMS           = 0;
+float I_MAX_BMS           = 0;
+float I_MIN_BMS           = 0;
 
-uint8_t P_MAX_BMS           = 0;
-uint8_t P_MIN_BMS           = 0;
+float P_MAX_BMS           = 0;
+float P_MIN_BMS           = 0;
 
-uint8_t T_MAX_BMS           = 0;
-uint8_t T_MIN_BMS           = 0;
+float T_MAX_BMS           = 0;
+float T_MIN_BMS           = 0;
 
-uint8_t V_CELDA_MAX_BMS     = 0;
-uint8_t V_CELDA_MIN_BMS     = 0;    
+float V_CELDA_MAX_BMS     = 0;
+float V_CELDA_MIN_BMS     = 0;    
 
-uint8_t NIV_BAT_MAX_BMS     = 0;
-uint8_t NIV_BAT_MIN_BMS     = 0;
-
-
-uint8_t V_BAT_MAX_DCDC      = 0;
-uint8_t V_BAT_MIN_DCDC      = 0;
-
-uint8_t T_MAX_DCDC          = 0;
-uint8_t T_MIN_DCDC          = 0;
+float NIV_BAT_MAX_BMS     = 0;
+float NIV_BAT_MIN_BMS     = 0;
 
 
-uint8_t VEL_MAX_INV         = 0;
-uint8_t VEL_MIN_INV         = 0;
+float V_BAT_MAX_DCDC      = 0;
+float V_BAT_MIN_DCDC      = 0;
 
-uint8_t T_MAX_MOTOR         = 0;
-uint8_t T_MIN_MOTOR         = 0;
+float T_MAX_DCDC          = 0;
+float T_MIN_DCDC          = 0;
 
-uint8_t V_MAX_INV           = 0;
-uint8_t V_MIN_INV           = 0;
 
-uint8_t I_MAX_INV           = 0;
-uint8_t I_MIN_INV           = 0;
+float  VEL_MAX_INV         = 0;
+float  VEL_MIN_INV         = 0;
 
-uint8_t T_MAX_INV           = 0;
-uint8_t T_MIN_INV           = 0;
+float T_MAX_MOTOR         = 0;
+float T_MIN_MOTOR         = 0;
 
-uint8_t P_MAX_INV           = 0;
-uint8_t P_MIN_INV           = 0;
+float V_MAX_INV           = 0;
+float V_MIN_INV           = 0;
+
+float I_MAX_INV           = 0;
+float I_MIN_INV           = 0;
+
+float T_MAX_INV           = 0;
+float T_MIN_INV           = 0;
+
+float P_MAX_INV           = 0;
+float P_MIN_INV           = 0;
 
 
 //estados posibles de cada parte
