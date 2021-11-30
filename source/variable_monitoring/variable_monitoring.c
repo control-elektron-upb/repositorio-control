e12cb9ed5e7ca73c81fbe0e0242aa25bf225dcfb
Andresfe <luqueandres55@gmail.com>
2021-11-30 08:53:54 -0500

    se elimino el estado default, y se corriegio la funcion de clasificacion, se realizaron las funciones para el dcdc y el inversor

--- source/variable_monitoring/variable_monitoring.c
+++ source/variable_monitoring/variable_monitoring.c
@@ -12,53 +12,29 @@
 #include "variable_monitoring.h"
 
 
-/**
- * @brief Estado por defecto de las variables
- * 
- */
-#define DEFAULT_STATE       DATA_PROBLEM
-
-/**
- * @brief Inicializa instancia de estructura de tipo bms_vars_states_t con los estados de las variables del BMS en DEFAULT_STATE
- * 
- */
-bms_vars_states_t       Bms_States = {DEFAULT_STATE};
-
-/**
- * @brief Inicializa instancia de estructura de tipo dcdc_vars_states_t con los estados de las variables del DCDC en DEFAULT_STATE
- * 
- */
-dcdc_vars_states_t      Dcdc_States = {DEFAULT_STATE};
-
-/**
- * @brief Inicializa instancia de estructura de tipo inversor_vars_states_t con los estados de las variables del inversor en DEFAULT_STATE
- * 
- */
-inversor_vars_states_t  Inversor_States = {DEFAULT_STATE};
-
 
 /**
  * @brief Función principal de monitoreo de variables
  * 
- *  Actualiza los estados de las variables decodificadas del BMS, DCDC e inversor.
- * 
  */
-void Variable_Monitoring (void)
+void variable_monitoring (void)
 {
-    bms_variable_monitoring(&Bms_States);               /* Actualiza estado de las variables del BMS */
-    dcdc_variable_monitoring(&Dcdc_States);             /* Actualiza estado de las variables del DCDC */
-    inversor_variable_monitoring(&Inversor_States);     /* Actualiza estado de las variables del inversor */
+
+    /** Actualiza estado de las variables de BMS */
+    bms_variable_monitoring(&Bms_States);
+
+    /** Actualiza estado de las variables de DCDC */
+    dcdc_variable_monitoring(&Dcdc_States);
+
+    /** Actualiza estado de las variables de Inversor */
+    inversor_variable_monitoring(&Inversor_States);
 }
 
 
 /**
  * @brief Monitoreo de las variables decodificadas del BMS
  * 
- * Actualiza los estados de las variables decodificadas del BMS de acuerdo a sus valores actuales, 
- * límites máximo y mínimo y porcentaje de zona regular. Dichos estados son guardados por referencia
- * en una estructura de tipo bms_vars_states_t.
- * 
- * @param p_Bms_States Puntero a estructura de tipo bms_vars_states_t que contiene los estados de las variables del BMS
+ * @param Bms_States Estructura con estados de las variables del BMS
  */
 static void bms_variable_monitoring(bms_vars_states_t *p_Bms_States)
 {
@@ -79,36 +55,28 @@ static void bms_variable_monitoring(bms_vars_states_t *p_Bms_States)
 /**
  * @brief Monitoreo de las variables decodificadas del DCDC
  * 
- * Actualiza los estados de las variables decodificadas del DCDC de acuerdo a sus valores actuales, 
- * límites máximo y mínimo y porcentaje de zona regular. Dichos estados son guardados por referencia
- * en la estructura de tipo dcdc_vars_states_t.
- * 
- * @param p_Dcdc_States Puntero a estructura de tipo dcdc_vars_states_t que contiene los estados de las variables del DCDC
+ * @param Dcdc_States Estructura con estados de las variables del DCDC
  */
 static void dcdc_variable_monitoring(dcdc_vars_states_t *p_Dcdc_States)
 {
     p_Dcdc_States->voltaje_bateria = current_var_state(V_BAT_MAX_DCDC, V_BAT_MIN_DCDC, bus_data.Rx_Dcdc.voltaje_bateria);
     
     p_Dcdc_States->t_max = current_var_state(T_MAX_DCDC, T_MIN_DCDC, bus_data.Rx_Dcdc.t_max);
+    
 }
 
-
 /**
- * @brief Monitoreo de las variables decodificadas del inversor
+ * @brief Monitoreo de las variables decodificadas del Inversor
  * 
- * Actualiza los estados de las variables decodificadas del inversor de acuerdo a sus valores actuales, 
- * límites máximo y mínimo y porcentaje de zona regular. Dichos estados son guardados por referencia
- * en la estructura de tipo inversor_vars_states_t.
- * 
- * @param p_Inversor_States Puntero a estructura de tipo inversor_vars_states_t que contiene los estados de las variables del inversor
+ * @param Inversor_States Estructura con estados de las variables del Inversor
  */
 static void inversor_variable_monitoring(inversor_vars_states_t *p_Inversor_States)
 {
     p_Inversor_States->velocidad = current_var_state(VEL_MAX_INV, VEL_MIN_INV, bus_data.Rx_Inversor.velocidad);
 
-    p_Inversor_States->V = current_var_state(V_MAX_INV, V_MIN_INV, bus_data.Rx_Inversor.V);
+    p_Inversor_States->V = current_var_state(V_MAX_INV, V_MIN_INV, bus_data.Rx_Inversor.voltaje);
     
-    p_Inversor_States->I = current_var_state(I_MAX_INV, I_MIN_INV, bus_data.Rx_Inversor.I);
+    p_Inversor_States->I = current_var_state(I_MAX_INV, I_MIN_INV, bus_data.Rx_Inversor.voltaje);
     
     p_Inversor_States->temp_max = current_var_state(T_MAX_INV, T_MIN_INV, bus_data.Rx_Inversor.temp_max);
     
@@ -118,27 +86,29 @@ static void inversor_variable_monitoring(inversor_vars_states_t *p_Inversor_Stat
 }
 
 
+
 /**
- * @brief Función para determinar el estado actual de la variable a evaluar
+ * @brief Función de comparaciones
  * 
  * @param D_max Valor máximo de la variable
  * @param D_min Valor mínimo de la variable
  * @param Data  Valor actual de la variable 
- * @return var_state Estado actual de la variable
+ * @return var_state 
  */
-static var_state current_var_state(rx_var_t D_max, rx_var_t D_min, rx_var_t Data)
-{
+static var_state comparaciones (float D_max, float D_min, float Data){
+
     if((Data < (D_max * (-REGULAR_ZONE + 1))) & (Data > (D_min*(REGULAR_ZONE + 1)))) {
         return OK; 
 
     }else if((Data > (D_max * (REGULAR_ZONE + 1))) | (Data < (D_min*(-REGULAR_ZONE + 1)))) {
-        return REGULAR; 
+        return PROBLEM; 
 
     }else if(((Data < (D_max * (REGULAR_ZONE + 1))) & (Data > (D_max * (-REGULAR_ZONE + 1))))  | 
     ((Data > (D_min*(-REGULAR_ZONE + 1))) & (Data < (D_min*(REGULAR_ZONE + 1))))){
-        return PROBLEM; 
+        return REGULAR; 
 
     }else{
         return DATA_PROBLEM;
+
     }
 }
